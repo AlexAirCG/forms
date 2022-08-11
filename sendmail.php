6cdf6dcb6973cb82517ec21ddf1401f553f1df4c
AlexAirCG <108254394+AlexAirCG@users.noreply.github.com>
2022-08-11 19:53:27 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,71 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('re', 'phpmailer/language/');
+	$mail->IsHTML(true);
+	
+	// от кого письмо
+	$mail->setForm('info@fls.guru', 'Фрилансер по жизни');
+	// кому отправить>
+	$mail->addAddress('code@fls.guru');
+	// тема письма
+	$mail->Subject = 'Привет, это фрилансер по жизни';
+
+	// пол
+	$hand = "Мужской";
+	if($_POST['hand'] == "left") {
+		$hand = "Женский";
+	}
+
+	// тело письма
+	$body = '<h1>встречайте супер письмо</h1>';
+
+	if(trim(!empty($_POST['name']))) {
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))) {
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))) {
+		$body.='<p><strong>Пол:</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))) {
+		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))) {
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+
+	// прикрепить файлы
+	if(!empty($_FILES['image']['tmp_name'])) {
+		// путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['tmp_name'];
+		// грузим файл
+		if(copy($_FILES['image']['tmp_name'], $filePath)) {
+			$fileAttach = $filePath;
+			$body.='<p><strong>фото в приложении</strong></p>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	// отправляем
+	if(!$mail->send()) {
+		$message = 'Ошибка';
+	}else{
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+
+	?>
